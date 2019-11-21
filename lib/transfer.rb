ce26256eecaac83a1ccbf6689fc3b436dea102c9
Kizito Njoku <nkizito57@gmail.com>
2019-11-21 15:45:26 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,9 +14,11 @@ class Transfer
     true if sender_receiver > 0
     receiver.valid?
     sender.valid?
-    
   end
   def execute_transaction
-    return sender.balance - amount 
+    sender.balance -= amount if receiver.balance += amount  and self.status = "complete"
+    sender.balance += amount if sender.balance < 950 
+    receiver.balance -= amount if receiver.balance > 1050 
+    'Transaction rejected. Please check your account balance.' if sender.status == "rejected"
   end 
 end
