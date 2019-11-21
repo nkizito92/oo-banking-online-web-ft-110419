6e8fd9c7fc606b3850a94126e75217536b6c5220
Kizito Njoku <nkizito57@gmail.com>
2019-11-21 04:58:09 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+  
+  def deposit(money)
+    # binding.pry
+    @balance += money
+  end 
+  
+  def display_balance 
+    "Your balance is $#{@balance}." 
+  end 
+  
+  def valid?
+    
+  end 
 end
